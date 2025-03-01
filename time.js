7fdd4b2a91850332591eea8c163c1cd78727320a
Abu Syeed Abdullah <72955464+asapial@users.noreply.github.com>
2025-03-01 17:08:08 +0600

    automated time is updated

--- time.js
+++ time.js
@@ -1,1 +1,19 @@
-const dateContainer=document.getElementById()
+const dateContainer=document.getElementById('datetime');
+
+const today = new Date();
+
+const formattedDate = today.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+});
+
+dateContainer.innerText=formattedDate;
+
+const weekContainer=document.getElementById('weekDay');
+
+const weekday = today.toLocaleDateString('en-US', {
+    weekday: 'short'
+});
+
+weekContainer.innerText=weekday;
